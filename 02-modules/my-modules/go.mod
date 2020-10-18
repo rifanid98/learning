c98ff80324c1146ce0713cf39b0a5a729e27b6ca
rifanid98 <adninsijawa.office@gmail.com>
2020-10-18 20:46:12 +0700

    -

---
+++ 02-modules/my-modules/go.mod
@@ -0,0 +1,5 @@
+module github.com/rifanid98/learning
+
+go 1.15
+
+require github.com/rifanid98/golang-modules v1.0.0
---
+++ 02-modules/my-modules/modules.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	mymodules "github.com/rifanid98/golang-modules"
+)
+
+func main() {
+	fmt.Println(mymodules.SayHello())
+}
