0075a72d90ff7fcbc7dc245c48371a4f0b387304
rifanid98 <adninsijawa.office@gmail.com>
2020-10-20 09:06:27 +0700

    new lessons

---
+++ fundamental/src/com/learning/DataTypePrimitive.java
@@ -0,0 +1,34 @@
+package com.learning;
+
+public class DataTypePrimitive {
+
+    public static void main(String[] args) {
+        int myInteger = 10;
+        /*
+        Primitive Data Type:
+        -   int
+        -   byte
+        -   short
+        -   long
+        -   double
+        -   float
+        -   char
+        -   boolean
+         */
+        System.out.println("=========INTEGER==========");
+        System.out.println("nilai integer " + myInteger);
+        System.out.println("nilai max     " + Integer.MAX_VALUE);
+        System.out.println("besar integer " + Integer.BYTES);
+        System.out.println("besar integer " + Integer.BYTES);
+        System.out.println("nilai min     " + Integer.MIN_VALUE);
+        System.out.println("besar integer " + Integer.SIZE);
+
+        System.out.println("=========BYTE==========");
+        System.out.println("nilai integer " + myInteger);
+        System.out.println("nilai max     " + Integer.MAX_VALUE);
+        System.out.println("besar integer " + Integer.BYTES);
+        System.out.println("besar integer " + Integer.BYTES);
+        System.out.println("nilai min     " + Integer.MIN_VALUE);
+        System.out.println("besar integer " + Integer.SIZE);
+    }
+}
---
+++ fundamental/src/com/learning/VariableDeclaration.java
@@ -0,0 +1,15 @@
+package com.learning;
+
+public class VariableDeclaration {
+
+    public static void main(String[] args) {
+        String name;
+        name = "Adnin";
+        int age = 22;
+        String address = "Kp. Pasar Rebo";
+        boolean married = false;
+
+        System.out.println(name);
+        System.out.println("Variable");
+    }
+}
