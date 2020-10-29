ec4a71ba0727735d85e51522d927f0e6ebb34b80
rifanid98 <adninsijawa.office@gmail.com>
2020-10-29 11:13:03 +0700

    new lessons

--- fundamental/src/App.vue
+++ fundamental/src/App.vue
@@ -6,7 +6,8 @@
         <!-- <E04ConditionalRendering /> -->
         <!-- <E05ForLoop /> -->
         <!-- <E06Component /> -->
-        <E07LifeCycle />
+        <!-- <E07LifeCycle /> -->
+        <E08VOnce />
     </div>
 </template>
 
@@ -17,7 +18,8 @@
 // import E04ConditionalRendering from "./components/E04ConditionalRendering.vue";
 // import E05ForLoop from "./components/E05ForLoop.vue";
 // import E06Component from "./components/E06Component.vue";
-import E07LifeCycle from "./components/E07LifeCycle.vue";
+// import E07LifeCycle from "./components/E07LifeCycle.vue";
+import E08VOnce from "./components/E08VOnce.vue";
 
 const App = {
     name: 'App',
@@ -28,7 +30,8 @@ const App = {
         // E04ConditionalRendering,
         // E05ForLoop,
         // E06Component,
-        E07LifeCycle
+        // E07LifeCycle,
+        E08VOnce
     }
 }
 
---
+++ fundamental/src/components/E08VOnce.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <p>berubah: {{ message }}</p>
+        <p v-once>tetap: {{ message }}</p>
+        <button @click="message = !message">change message</button>
+    </div>
+</template>
+
+<script>
+const E08VOnce = {
+    name: 'E08VOnce',
+    data() {
+        return {
+            message: true
+        }
+    }
+}
+
+export default E08VOnce;
+</script>
