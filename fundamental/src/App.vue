f1c37b29b37848736e81dccdfc54e1fe3359bb09
rifanid98 <adninsijawa.office@gmail.com>
2020-10-29 10:07:19 +0700

    new lessons

--- fundamental/src/App.vue
+++ fundamental/src/App.vue
@@ -6,6 +6,7 @@
         <!-- <E04ConditionalRendering /> -->
         <!-- <E05ForLoop /> -->
         <!-- <E06Component /> -->
+        <E07LifeCycle />
     </div>
 </template>
 
@@ -15,17 +16,19 @@
 // import E03VModel from "./components/E03VModel.vue";
 // import E04ConditionalRendering from "./components/E04ConditionalRendering.vue";
 // import E05ForLoop from "./components/E05ForLoop.vue";
-import E06Component from "./components/E06Component.vue";
+// import E06Component from "./components/E06Component.vue";
+import E07LifeCycle from "./components/E07LifeCycle.vue";
 
 const App = {
     name: 'App',
     components: {
         // E01DeclarativeRendering,
         // E02Methods,
-        // E03VModel
-        // E04ConditionalRendering
-        // E05ForLoop
-        // E06Component
+        // E03VModel,
+        // E04ConditionalRendering,
+        // E05ForLoop,
+        // E06Component,
+        E07LifeCycle
     }
 }
 
