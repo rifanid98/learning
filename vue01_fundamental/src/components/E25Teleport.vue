fa94f95361f70c34b9853612d6138d0fcd5be48a
rifanid98 <adninsijawa.office@gmail.com>
2020-11-10 13:28:40 +0700

    new lessons

---
+++ vue01_fundamental/src/components/E25Teleport.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <p>Teleport Component</p>
+        <!-- <teleport to="#teleport">
+            <p>ini juga bisa diteleport</p>
+        </teleport> -->
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'E25Teleport',
+    data() {
+        return {
+
+        }
+    }
+}
+</script>
