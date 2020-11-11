78fe93d5d675a077e06dd746606fcb09cfa6be92
rifanid98 <adninsijawa.office@gmail.com>
2020-11-11 13:06:28 +0700

    new lessons

--- vue01_fundamental/src/App.vue
+++ vue01_fundamental/src/App.vue
@@ -1,46 +1,83 @@
 <template>
-    <div>
-        <!-- E21PropTypes -->
-            <!-- use default prop value -->
-            <!-- <E21PropTypes /> -->
-            <!-- use custom props value / props assignment -->
-            <!-- <E21PropTypes name="Adnin" :age="22" /> -->
-
-        <!-- E22ProvideInject -->
-            <!-- <E22ProvideInject /> -->
-
-        <!-- E23KeepAlive -->
-            <!-- <E23KeepAlive /> -->
-
-        <!-- E24Mixins -->
-            <!-- <E24Mixins /> -->
-
-        <!-- E25Teleport -->
-            <teleport to="#teleport">
-                <E25Teleport />
-            </teleport>
+    <div class="container">
+        <div class="card mt-4">
+            <div class="card-body">
+                <h5 class="card-title">SIMPLE TODO APP</h5>
+                <div class="row">
+                    <div class="col-10">
+                        <input v-model="todo" @keyup.enter="addTodo" type="text" class="form-control" required="required">
+                    </div>
+                    <div class="col-2">
+                        <button @click="addTodo" class="btn btn-success">ADD</button>
+                    </div>
+                </div>
+                <div>
+                    <div class="list-group">
+                        <div v-for="todo in todos" :key="todo" class="list-group-item d-flex flex-row justify-content-between">
+                            <p>{{ todo }}</p>
+                            <button @click="deleteTodo(todo)" type="button" class="btn btn-outline-danger">X</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
-// import E21PropTypes from './components/E21PropTypes.vue';
-// import E22ProvideInject from './components/E22ProvideInject.vue';
-// import E23KeepAlive from './components/E23KeepAlive.vue';
-// import E24Mixins from './components/E24Mixins.vue';
-import E25Teleport from './components/E25Teleport.vue';
-
 export default {
     name: 'App',
-    components: {
-        // E21PropTypes,
-        // E22ProvideInject,
-        // E23KeepAlive,
-        // E24Mixins,
-        E25Teleport
-    },
     data() {
         return {
-            
+            todo: '',
+            todos: []
+        }
+    },
+    methods: {
+        addTodo() {
+            if (!this.validate()) {
+                alert(`Todo must be at least 3 characters`);
+                return;
+            }
+            if (this.todoExists()) {
+                alert(`Todo ${this.todo} is already added`);
+                return;
+            }
+            this.todos.push(this.todo);
+            this.clean();
+        },
+        deleteTodo(value) {
+            // yang diharapkan
+            let todos = this.todos;
+            this.todos.forEach((todo, index) => {
+                if (todo === value) {
+                    todos.splice(index, 1);
+                }
+            });
+            this.todos = todos;
+
+            // yang terjadi, apakah ini pass by reference di vue?
+            let todos = this.todos;
+            todos.forEach((todo, index) => {
+                if (todo === value) {
+                    todos.splice(index, 1);
+                }
+            });
+        },
+        todoExists() {
+            if (this.todos.find(todo => todo === this.todo)) {
+                return true;
+            }
+        },
+        validate() {
+            if (this.todo !== '' &&  this.todo !== null && this.todo.length >= 3) {
+                return true;
+            }
+
+            return false;
+        },
+        clean() {
+            this.todo = [];
         }
     }
 }
