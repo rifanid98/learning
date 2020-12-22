4fb8e847914511d7ade36c5708bb1c2a79cf1d71
rifanid98 <adninsijawa.office@gmail.com>
2020-12-22 14:07:18 +0700

    new lessons

---
+++ src/com/java06_lambda_expression/lesson01_membuat_lambda/lambda_interface/SimpleAction.java
@@ -0,0 +1,6 @@
+package com.java06_lambda_expression.lesson01_membuat_lambda.lambda_interface;
+
+@FunctionalInterface
+public interface SimpleAction {
+    String action();
+}
---
+++ src/com/java06_lambda_expression/lesson01_membuat_lambda/lambda_interface/SimpleActionParam.java
@@ -0,0 +1,6 @@
+package com.java06_lambda_expression.lesson01_membuat_lambda.lambda_interface;
+
+@FunctionalInterface
+public interface SimpleActionParam {
+    String action(String name);
+}
