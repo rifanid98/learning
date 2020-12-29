9ff21b7bb71571cf085003b83a4d4ee3cf1f00ed
rifanid98 <adninsijawa.office@gmail.com>
2020-12-29 09:12:11 +0700

    assertions

--- src/com/java08_java_unit_test/unit_test/src/test/java/unit/test/CalculatorTest.java
+++ src/com/java08_java_unit_test/unit_test/src/test/java/unit/test/CalculatorTest.java
@@ -1,6 +1,8 @@
 package unit.test;
 
+import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
+
 
 public class CalculatorTest {
     private Calculator calculator = new Calculator();
@@ -8,6 +10,7 @@ public class CalculatorTest {
     @Test
     public void addTest() {
         var result = calculator.add(10, 10);
+        assertEquals(20, result);
     }
 }
 
