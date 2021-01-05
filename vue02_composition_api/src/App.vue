4534d16d9ba66e2cf4385c95aa8e1554c73f9ba5
rifanid98 <adninsijawa.office@gmail.com>
2021-01-05 13:54:33 +0700

    Lesson03ToRefs

--- vue02_composition_api/src/App.vue
+++ vue02_composition_api/src/App.vue
@@ -2,19 +2,22 @@
   <div>
     <!-- <Lesson01RefVue /> -->
     <!-- <Lesson02ReactiveVue /> -->
+    <Lesson03ToRefsVue />
   </div>
 </template>
 
 <script>
 // import Lesson01RefVue from './components/Lesson01Ref.vue';
 // import Lesson02ReactiveVue from './components/Lesson02Reactive.vue';
+import Lesson03ToRefsVue from './components/Lesson03ToRefs.vue'
 
 export default {
   setup() {
   },
   components: {
     // Lesson01RefVue,
-    // Lesson02ReactiveVue
+    // Lesson02ReactiveVue,
+    Lesson03ToRefsVue
   }
 }
 </script>
