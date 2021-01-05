f4f1b6ee155d822fac5b7e4ab6a99d0ce2753b00
rifanid98 <adninsijawa.office@gmail.com>
2021-01-05 13:21:59 +0700

    Ref

---
+++ vue02_composition_api/src/components/Lesson01Ref.vue
@@ -0,0 +1,21 @@
+<template>
+  <h1>{{ message }}</h1>
+</template>
+
+<script>
+import {ref} from "vue";
+
+export default {
+  setup() {
+    const message = ref("Hello World");
+    
+    setTimeout(() => {
+      message.value = "Hallo Dunia";
+    }, 500);
+    
+    return {
+      message
+    }
+  }
+}
+</script>
