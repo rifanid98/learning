d13a93858fc425afe3849b707337b022f57a695b
rifanid98 <adninsijawa.office@gmail.com>
2021-01-05 14:06:35 +0700

    Lesson04Methods

--- vue02_composition_api/src/App.vue
+++ vue02_composition_api/src/App.vue
@@ -2,14 +2,16 @@
   <div>
     <!-- <Lesson01RefVue /> -->
     <!-- <Lesson02ReactiveVue /> -->
-    <Lesson03ToRefsVue />
+    <!-- <Lesson03ToRefsVue /> -->
+    <Lesson04MethodsVue />
   </div>
 </template>
 
 <script>
 // import Lesson01RefVue from './components/Lesson01Ref.vue';
 // import Lesson02ReactiveVue from './components/Lesson02Reactive.vue';
-import Lesson03ToRefsVue from './components/Lesson03ToRefs.vue'
+// import Lesson03ToRefsVue from './components/Lesson03ToRefs.vue'
+import Lesson04MethodsVue from './components/Lesson04Methods.vue'
 
 export default {
   setup() {
@@ -17,7 +19,8 @@ export default {
   components: {
     // Lesson01RefVue,
     // Lesson02ReactiveVue,
-    Lesson03ToRefsVue
+    // Lesson03ToRefsVue,
+    Lesson04MethodsVue
   }
 }
 </script>
