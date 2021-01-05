381dd8f8c2741499ec8b31023b21e70066af2034
rifanid98 <adninsijawa.office@gmail.com>
2021-01-05 13:09:09 +0700

    initial commit

---
+++ vue02_composition_api/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <h1>{{ message }}</h1>
+</template>
+
+<script>
+
+export default {
+  setup() {
+    const message = "Hello World";
+
+    return {
+      message
+    }
+  }
+}
+</script>
