a26d648f6dd5ba0a16129e34fca35c45f859e3cd
rifanid98 <adninsijawa.office@gmail.com>
2021-01-05 13:38:53 +0700

    update

--- vue02_composition_api/src/App.vue
+++ vue02_composition_api/src/App.vue
@@ -1,20 +1,20 @@
 <template>
-  <Lesson01RefVue />
+  <div>
+    <!-- <Lesson01RefVue /> -->
+    <!-- <Lesson02ReactiveVue /> -->
+  </div>
 </template>
 
 <script>
-import Lesson01RefVue from './components/Lesson01Ref.vue';
+// import Lesson01RefVue from './components/Lesson01Ref.vue';
+// import Lesson02ReactiveVue from './components/Lesson02Reactive.vue';
 
 export default {
   setup() {
-    const message = "Hello World";
-
-    return {
-      message
-    }
   },
   components: {
-    Lesson01RefVue
+    // Lesson01RefVue,
+    // Lesson02ReactiveVue
   }
 }
 </script>
