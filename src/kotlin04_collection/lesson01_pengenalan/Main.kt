51676959791389d9fa02d0c9c6fe772530d75977
rifanid98 <adninsijawa.office@gmail.com>
2021-01-20 21:12:02 +0700

    pengenalan collection

---
+++ src/kotlin04_collection/lesson01_pengenalan/Main.kt
@@ -0,0 +1,21 @@
+package kotlin04_collection.lesson01_pengenalan
+
+/**
+ * Pengenalan Collection
+ *
+ * - Collection adalah hal umum yang biasa dimiliki di bahasa pemrograman,
+ *   seperti Java, Python, Ruby dan lain-lain.
+ * - Sebuah collection biasanya berisikan sejumlah data, bahkan bisa
+ *   kosong.
+ * - Kotlin standard library menyediakan data Collection yang sudah bisa
+ *   langsung kita gunakan, tanpa tambahan library.
+ */
+
+/**
+ * Tipe Collection
+ *
+ * - Secara garis besar, ada 2 jenis collection di Kotlin:
+ *      1. Immutable, collection yang hanya mendukung operasi read only.
+ *      2. Mutable, collection turunan immutable, namun mendukung operasi
+ *         write juga (add, edit, dan remove).
+ */
