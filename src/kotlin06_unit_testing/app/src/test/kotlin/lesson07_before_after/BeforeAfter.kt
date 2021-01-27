8859fe3e44cf042196abb690c2fd2c8ad520d7c0
rifanid98 <adninsijawa.office@gmail.com>
2021-01-28 06:37:21 +0700

    update descriptions

--- src/kotlin06_unit_testing/app/src/test/kotlin/lesson07_before_after/BeforeAfter.kt
+++ src/kotlin06_unit_testing/app/src/test/kotlin/lesson07_before_after/BeforeAfter.kt
@@ -2,24 +2,19 @@ package lesson07_before_after
 
 import org.junit.jupiter.api.*
 
-/**
- * Sebelum & Sedudah Unit Test
- *
- * - Kadang kita ingin menjalankan kode yang sama sebelum dan setelah eksekusi
- *   unit test.
- * - Hal ini sebenarnya bisa dilakukan secara manual di function @Test nya, namun
- *   hal ini akan membuat kode duplikat banyak sekali.
- * - JUnit memiliki annotation @BeforeEach dan @AfterEach.
- * - @BeforeEach digunakan untuk menandai function yang akan dieksekusi sebelum
- *   unit test dijalankan.
- * - @AfterEach digunakan untuk menandai function yang akan dieksekusi setelah
- *   unit teset dijalankan.
- * - Ingat, bahwa ini akan selalu dieksekusi setiap kali untuk function @Test,
- *   bukan sekali untuk class test saja.
- */
 @DisplayName("Before & After Each")
 class BeforeAfter {
 
+    /**
+     * Sebelum & Setelah Semua Unit Test
+     *
+     * - @BeforeEach & @AfterEach akan dieksekusi setiap kali function @Test jalan.
+     * - Namun kadang kita ingin melakukan sesuatu sebelum semua unit test berjalan,
+     *   atau setelah semua unit test berjalan.
+     * - Ini bisa dilakukan menggunakan annotation @BeforeAll dan @AfterAll.
+     * - Namun hanya static function (object function di kotlin) yang bisa menggunakan
+     *   @BeforeAll dan @AfterAll.
+     */
     companion object {
         @BeforeAll
         @JvmStatic
@@ -34,6 +29,21 @@ class BeforeAfter {
         }
     }
 
+    /**
+     * Sebelum & Sedudah Unit Test
+     *
+     * - Kadang kita ingin menjalankan kode yang sama sebelum dan setelah
+     *   eksekusi unit test.
+     * - Hal ini sebenarnya bisa dilakukan secara manual di function @Test nya,
+     *   namun hal ini akan membuat kode duplikat banyak sekali.
+     * - JUnit memiliki annotation @BeforeEach dan @AfterEach.
+     * - @BeforeEach digunakan untuk menandai function yang akan dieksekusi
+     *   sebelum unit test dijalankan.
+     * - @AfterEach digunakan untuk menandai function yang akan dieksekusi setelah
+     *   unit teset dijalankan.
+     * - Ingat, bahwa ini akan selalu dieksekusi setiap kali untuk function @Test,
+     *   bukan sekali untuk class test saja.
+     */
     @BeforeEach
     fun beforeEach(): Unit {
         println("Sebelum Unit Test")
@@ -45,7 +55,7 @@ class BeforeAfter {
     }
 
     @Test
-    fun test1() {
+a    fun test1() {
         println("Test 1")
     }
 
