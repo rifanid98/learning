b91da69398971cdd7772351b723c2d6f189bad96
rifanid98 <adninsijawa.office@gmail.com>
2021-02-04 16:07:48 +0700

    kotlin08_restful_api

---
+++ src/kotlin08_restful_api/src/main/kotlin/controller/Kotlin08RestfulApiApplication.kt
@@ -0,0 +1,11 @@
+package controller
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Kotlin08RestfulApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<Kotlin08RestfulApiApplication>(*args)
+}
