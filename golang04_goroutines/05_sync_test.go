3eae0333de7c58b858fd2d38fafe9f539f4cee26
rifanid <adninsijawa.office@gmail.com>
2021-04-28 13:28:59 +0700

    add sync.WaitGroup

--- golang04_goroutines/05_sync_test.go
+++ golang04_goroutines/05_sync_test.go
@@ -142,5 +142,34 @@ func TestDeadLock(t *testing.T) {
 
 	fmt.Println("User 1 ", user1.Name, " balance ", user1.Balance)
 	fmt.Println("User 2 ", user2.Name, " balance ", user2.Balance)
+}
 
+// # sync.WaitGroup
+// - WaitGroup adalah fitur yang bisa digunakan untuk menunggu sebuah
+// 	 proses selesai dilakukan
+// - Hal ini kadang diperlukan, misal kita ingin menjalankan beberapa
+// 	 proses menggunakan goroutine, tapi kita ingin semua proses selesai
+// 	 terlebih dahulu sebelum aplikasi kita selesai
+// - Kasus seperti ini bisa menggunakan WaitGroup
+// - Untuk menandai bahwa ada proses goroutine, kita bisa menggunakan
+// 	 method Add(int), setelah proses goroutine selesai, kita bisa gunakan method Done()
+// - Untuk menunggu semua proses selesai, kita bisa menggunakan method Wait()
+
+func runAsynchronous(group *sync.WaitGroup) {
+	defer group.Done()
+
+	group.Add(1)
+	fmt.Println("Hello")
+	time.Sleep(1 * time.Second)
+}
+
+func TestSyncWaitGroup(t *testing.T) {
+	group := &sync.WaitGroup{}
+
+	for i := 0; i < 100; i++ {
+		go runAsynchronous(group)
+	}
+
+	group.Wait()
+	fmt.Println("Complete")
 }
