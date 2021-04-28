9671db25f52719f2d6d32799ce24dccf2cb04faf
rifanid <adninsijawa.office@gmail.com>
2021-04-28 09:23:36 +0700

    add buffered channel

--- golang04_goroutines/03_channel_test.go
+++ golang04_goroutines/03_channel_test.go
@@ -109,3 +109,44 @@ func TestChannelOnlyInOnlyOut(t *testing.T) {
 	fmt.Println(name)
 	close(channel)
 }
+
+// # Buffered Channel
+// - Seperti yang dijelaskan sebelumnya, bahwa secara default channel
+// 	 itu hanya bisa menerima 1 data
+// - Artinya jika kita menambah data ke-2, maka kita akan diminta menunggu
+// 	 sampai data ke-1 ada yang mengambil
+// - Kadang-kadang ada kasus dimana pengirim lebih cepat dibanding penerima,
+// 	 dalam hal ini jika kita menggunakan channel, maka otomatis pengirim akan
+// 	 ikut lambat juga
+// - Untuknya ada Buffered Channel, yaitu buffer yang bisa digunakan untuk
+// 	 menampung data antrian di Channel
+
+// # Buffer Capacity
+// - Kita bebas memasukkan berapa jumlah kapasitas antrian di dalam buffer
+// - Jika kita set misal 5, artinya kita bisa menerima 5 data di buffer.
+// - Jika kita mengirim data ke 6, maka kita diminta untuk menunggu sampai
+// 	 buffer ada yang kosong
+// - Ini cocok sekali ketika memang goroutine yang menerima data lebih lambat
+// 	 dari yang mengirim data
+
+func TestBufferedChannel(t *testing.T) {
+	channel := make(chan string, 3)
+	defer close(channel)
+
+	go func() {
+		channel <- "Data 1"
+		channel <- "Data 2"
+		channel <- "Data 3"
+	}()
+
+	go func() {
+		fmt.Println("data 1 = ", <-channel)
+		fmt.Println("data 2 = ", <-channel)
+		fmt.Println("data 3 = ", <-channel)
+	}()
+
+	time.Sleep(1 * time.Second)
+
+	fmt.Println("channel cpacity ", cap(channel))
+	fmt.Println("channel length of stored data ", len(channel))
+}
