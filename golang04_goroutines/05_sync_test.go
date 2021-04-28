8ca1c1442442c91bd6998118db48b809c2f578dd
rifanid <adninsijawa.office@gmail.com>
2021-04-28 10:20:59 +0700

    add sync.RWMutex

--- golang04_goroutines/05_sync_test.go
+++ golang04_goroutines/05_sync_test.go
@@ -37,3 +37,46 @@ func TestSyncMutex(t *testing.T) {
 	time.Sleep(7 * time.Second)
 	fmt.Println("Counter x ", x)
 }
+
+// # sync.RWMutex
+// - Kadang ada kasus dimana kita ingin melakukan locking tidak hanya
+// 	 pada proses mengubah data, tapi juga membaca data
+// - Kita sebenarnya bisa menggunakan Mutex saja, namun masalahnya nanti
+// 	 akan rebutan antara proses membaca dan mengubah
+// - Di Go-Lang telah disediakan struct RWMutex (Read Write Mutex) untuk
+// 	 menangani hal ini, dimana Mutex jenis ini memiliki dua lock, lock untuk Read dan lock untuk Write
+
+type BankAccount struct {
+	RWMutex sync.RWMutex
+	Balance int
+}
+
+func (account *BankAccount) AddBalance(amount int) {
+	account.RWMutex.Lock()
+	account.Balance = account.Balance + amount
+	account.RWMutex.Unlock()
+}
+
+func (account *BankAccount) GetBalance() int {
+	account.RWMutex.RLock()
+	balance := account.Balance
+	account.RWMutex.RUnlock()
+	return balance
+}
+
+func TestSyncRWMutex(t *testing.T) {
+	account := BankAccount{}
+
+	for i := 0; i < 1000; i++ {
+		go func() {
+			for j := 0; j < 100; j++ {
+				account.AddBalance(1)
+				fmt.Println(account.GetBalance())
+			}
+		}()
+
+	}
+
+	time.Sleep(7 * time.Second)
+	fmt.Println("Final Balance : ", account.GetBalance())
+}
