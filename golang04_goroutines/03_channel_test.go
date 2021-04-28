9a4602dd01ccf36690c7e79ac055acde2b664de3
rifanid <adninsijawa.office@gmail.com>
2021-04-28 09:41:34 +0700

    add default select

--- golang04_goroutines/03_channel_test.go
+++ golang04_goroutines/03_channel_test.go
@@ -213,3 +213,41 @@ func TestSelectChannel(t *testing.T) {
 		}
 	}
 }
+
+// # Default Select
+// - Apa yang terjadi jika kita melakukan select terhadap channel yang
+// 	 ternyata tidak ada datanya?
+// - Maka kita akan menunggu sampai data ada
+// - Kadang mungkin kita ingin melakukan sesuatu jika misal semua channel
+// 	 tidak ada datanya ketika kita melakukan select channel
+// - Dalam select, kita bisa menambahkan default, dimana ini akan dieksekusi
+// 	 jika memang di semua channel yang kita select tidak ada datanya
+
+func TestDefaultSelectChannel(t *testing.T) {
+	channel1 := make(chan string)
+	channel2 := make(chan string)
+
+	defer close(channel1)
+	defer close(channel2)
+
+	go OnlyIn(channel1, "Data channel 1")
+	go OnlyIn(channel2, "Data channel 2")
+
+	counter := 0
+	for {
+		select {
+		case data := <-channel1:
+			fmt.Println("Data dari channel 1 ", data)
+			counter++
+		case data := <-channel2:
+			fmt.Println("Data dari channel 2 ", data)
+			counter++
+		default:
+			fmt.Println("Menunggu data dari channel")
+		}
+
+		if counter >= 2 {
+			break
+		}
+	}
+}
