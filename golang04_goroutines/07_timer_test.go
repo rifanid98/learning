a7ff8f826507f59ea3959b60fb8cb4ead8213001
rifanid <adninsijawa.office@gmail.com>
2021-04-29 08:39:53 +0700

    time.After

--- golang04_goroutines/07_timer_test.go
+++ golang04_goroutines/07_timer_test.go
@@ -11,10 +11,22 @@ import (
 // - Ketika waktu timer sudah expire, maka event akan dikirim ke dalam channel
 // - Untuk membuat Timer kita bisa menggunakan time.NewTimer(duration)
 
-func TestTimer(t *testing.T) {
+func TestTimeTimer(t *testing.T) {
 	timer := time.NewTimer(5 * time.Second)
 	fmt.Println(time.Now())
 
 	time := <-timer.C
+	fmt.Println(time)
+}
+
+// # time.After
+// - Kadang kita hanya butuh channel nya saja, tidak membutuhkan data Timer nya
+// - Untuk melakukan hal itu kita bisa menggunakan function time.After(duration)
+
+func TestTimeAfter(t *testing.T) {
+	channel := time.After(5 * time.Second)
+	fmt.Println(time.Now())
+
+	time := <-channel
 	fmt.Println(time)
 }
