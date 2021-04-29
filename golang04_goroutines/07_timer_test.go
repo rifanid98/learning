57a284ab5c1a4d24976fe205067d9010c92a3978
rifanid <adninsijawa.office@gmail.com>
2021-04-29 08:54:14 +0700

    time.NewTicker

--- golang04_goroutines/07_timer_test.go
+++ golang04_goroutines/07_timer_test.go
@@ -53,3 +53,21 @@ func TestTimeAfterFunc(t *testing.T) {
 
 	group.Wait()
 }
+
+// # time.Ticker
+// - Ticker adalah representasi kejadian yang berulang
+// - Ketika waktu ticker sudah expire, maka event akan dikirim ke dalam channel
+// - Untuk membuat ticker, kita bisa menggunakan time.NewTicker(duration)
+// - Untuk menghentikan ticker, kita bisa menggunakan Ticker.Stop()
+
+func TestTimeTicker(t *testing.T) {
+	ticker := time.NewTicker(1 * time.Second)
+
+	time.AfterFunc(5*time.Second, func() {
+		ticker.Stop()
+	})
+
+	for tick := range ticker.C {
+		fmt.Println(tick)
+	}
+}
