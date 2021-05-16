7c2311719ddb73a47b29f38ae794ca5974469e1d
rifanid <adninsijawa.office@gmail.com>
2021-05-16 14:02:12 +0700

    server

--- golang08_web/server_test.go
+++ golang08_web/server_test.go
@@ -5,6 +5,15 @@ import (
 	"testing"
 )
 
+// # Server
+// - Server adalah struct yang terdapat di package net/http yang digunakan sebagai
+// 	 representasi Web Server di Go-Lang
+// - Untuk membuat web, kita wajib membuat Server
+// - Saat membuat data Server, ada beberapa hal yang perlu kita tentukan, seperti
+// 	 host dan juga port tempat dimana Web kita berjalan
+// - Setelah membuat Server, kita bisa menjalankan Server tersebut menggunakan
+// 	 function ListenAndServe()
+
 func TestServer(t *testing.T) {
 	server := http.Server{
 		Addr: "localhost:8080",
