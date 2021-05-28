e4958e20b169d485ac489f4108475dea5ba71ed7
rifanid <adninsijawa.office@gmail.com>
2021-05-28 17:40:51 +0700

    upload file

--- golang08_web/21_upload_file_test.go
+++ golang08_web/21_upload_file_test.go
@@ -1,8 +1,13 @@
 package golang08_web
 
 import (
+	"bytes"
+	_ "embed"
+	"fmt"
 	"io"
+	"mime/multipart"
 	"net/http"
+	"net/http/httptest"
 	"os"
 	"testing"
 	"text/template"
@@ -24,37 +29,37 @@ import (
 // 	 multipart.File sebagai representasi file nya, dan multipart.FileHeader sebagai
 // 	 informasi file nya
 
+func uploadForm(rw http.ResponseWriter, r *http.Request) {
+	err := myTemplates2.ExecuteTemplate(rw, "upload.form.gohtml", nil)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func uploadProcess(rw http.ResponseWriter, r *http.Request) {
+	// r.ParseMultipartForm(32 << 20)
+	file, fileHeader, err := r.FormFile("file")
+	if err != nil {
+		panic(err)
+	}
+	fileDestination, err := os.Create("./resources/" + fileHeader.Filename)
+	if err != nil {
+		panic(err)
+	}
+	_, err = io.Copy(fileDestination, file)
+	if err != nil {
+		panic(err)
+	}
+	name := r.PostFormValue("name")
+	myTemplates2.ExecuteTemplate(rw, "upload.success.gohtml", map[string]interface{}{
+		"Name": name,
+		"File": "/static/" + fileHeader.Filename,
+	})
+}
+
 var myTemplates2 = template.Must(template.ParseFS(embedTemplates, "templates/*.gohtml"))
 
 func TestUploadFile(t *testing.T) {
-	uploadForm := func(rw http.ResponseWriter, r *http.Request) {
-		err := myTemplates2.ExecuteTemplate(rw, "upload.form.gohtml", nil)
-		if err != nil {
-			panic(err)
-		}
-	}
-
-	uploadProcess := func(rw http.ResponseWriter, r *http.Request) {
-		// r.ParseMultipartForm(32 << 20)
-		file, fileHeader, err := r.FormFile("file")
-		if err != nil {
-			panic(err)
-		}
-		fileDestination, err := os.Create("./resources/" + fileHeader.Filename)
-		if err != nil {
-			panic(err)
-		}
-		_, err = io.Copy(fileDestination, file)
-		if err != nil {
-			panic(err)
-		}
-		name := r.PostFormValue("name")
-		myTemplates2.ExecuteTemplate(rw, "upload.success.gohtml", map[string]interface{}{
-			"Name": name,
-			"File": "/static/" + fileHeader.Filename,
-		})
-	}
-
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
 		http.Redirect(rw, r, "/form", http.StatusPermanentRedirect)
@@ -72,4 +77,28 @@ func TestUploadFile(t *testing.T) {
 	if err != nil {
 		panic(err)
 	}
+}
+
+//go:embed resources/dicoding.png
+var logo []byte
+
+func TestUploadFileTesting(t *testing.T) {
+	payload := new(bytes.Buffer)
+
+	writer := multipart.NewWriter(payload)
+	writer.WriteField("name", "Adnin Rifandi Sutanto Putra")
+
+	file, _ := writer.CreateFormFile("file", "NewLogo.png")
+	file.Write(logo)
+	writer.Close()
+
+	request := httptest.NewRequest(http.MethodPost, "http://localhost:8080/", payload)
+	request.Header.Set("Content-Type", writer.FormDataContentType())
+
+	recorder := httptest.NewRecorder()
+
+	uploadProcess(recorder, request)
+
+	body, _ := io.ReadAll(recorder.Result().Body)
+	fmt.Println(string(body))
 }
